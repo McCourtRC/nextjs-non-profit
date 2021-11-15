3a6c290cf4bc35569e6ba9d06a4ad98aa760186e
Corey McCourt <mccourtrc@gmail.com>
2021-11-15 09:43:32 -0800

    query projects in Projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,10 +1,45 @@
-import type { NextPage } from 'next';
 import PageTitle from 'lib/PageTitle';
+import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useQuery, gql } from '@apollo/client';
+import ProjectList from 'components/ProjectList';
+import { Project } from 'types';
+import ProjectListItem from 'components/ProjectListItem';
+
+interface GetProjects {
+  projects: Project[];
+}
+
+const GET_PROJECTS = gql`
+  query AllProjects {
+    projects {
+      _id
+      title
+      description
+    }
+  }
+`;
 
 const Projects: NextPage = () => {
+  const { data: session } = useSession();
+  const { data, loading, error } = useQuery<GetProjects>(GET_PROJECTS);
+  const { projects } = data ?? {};
+
+  if (!session)
+    return (
+      <>
+        <PageTitle>Must be logged in</PageTitle>
+      </>
+    );
+
   return (
     <>
       <PageTitle>Projects</PageTitle>
+      <ProjectList>
+        {projects?.map((project) => (
+          <ProjectListItem key={`project-${project._id}`} project={project} />
+        ))}
+      </ProjectList>
     </>
   );
 };
