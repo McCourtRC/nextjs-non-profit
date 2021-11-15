ccbd5d3fe9673e9342bb88ad982638ab902fc701
Corey McCourt <mccourtrc@gmail.com>
2021-11-15 09:47:07 -0800

    use auth session to protect api route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+type Error = {
+  error: string;
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Error>
 ) {
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(403).send({ error: 'Must be signed in' });
+  }
+
   res.status(200).json({ name: 'John Doe' });
 }
