ac1b93ee0b1c628fe579d099463b80741e7aa795
Corey McCourt <mccourtrc@gmail.com>
2021-11-15 09:41:02 -0800

    use ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import ApolloProvider from 'components/ApolloProvider';
 import Layout from 'components/Layout';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ApolloProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
     </SessionProvider>
   );
 }
